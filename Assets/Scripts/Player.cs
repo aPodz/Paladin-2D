37542f7c330ded38f651b7bfc07a1dd475b6bff8
aPodz <adam.podzim@gmail.com>
2024-02-08 15:55:33 +0100

    Created an idle blend and coded the idle facing direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRigidBody;
-
+    [SerializeField] Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,16 @@ public class Player : MonoBehaviour
         float verticalMovement = Input.GetAxisRaw("Vertical");
 
         playerRigidBody.velocity = new Vector2 (horizontalMovement, verticalMovement);
-    }
+
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+
+        if (horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement); 
+        }
+
+        
+    }   
 }
