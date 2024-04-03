f29a243750e721871a28363c487cf8c435f0e685
aPodz <adam.podzim@gmail.com>
2024-04-03 23:06:45 +0200

    Code for fading during scene change finished

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class AreaExit : MonoBehaviour
 {
 
     [SerializeField] string sceneToLoad;
     [SerializeField] string transitionArea;
+    
   
     // Start is called before the first frame update
     void Start()
@@ -26,8 +28,15 @@ public class AreaExit : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Player.instance.playerTransition = transitionArea;
-            SceneManager.LoadScene(sceneToLoad);
+            MenuManager.instance.FadeImage();
+            StartCoroutine(LoadSceneCoroutine());
         }
         
+    }
+
+    IEnumerator LoadSceneCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
