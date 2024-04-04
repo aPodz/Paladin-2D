c18f2019e7e12b5a7d92879617cbfe8dee0b5354
aPodz <adam.podzim@gmail.com>
2024-04-04 10:15:47 +0200

    Added a dialog handler and created a test dialog with an NPC

---
+++ Assets/Scripts/Dialog/DialogController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class DialogController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI dialogText, nameText;
+    [SerializeField] GameObject dialogBox, nameBox;
+
+    [SerializeField] string[] dialogLines;
+    [SerializeField] int currentLine;
+
+    public static DialogController instance;
+    private bool dialogJustStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        dialogText.text = dialogLines[currentLine];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogBox.activeInHierarchy)
+        {           
+            if (Input.GetButtonUp("Fire1"))
+            {   
+                if (!dialogJustStarted)
+                {
+                    currentLine++;
+                    if (currentLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+                    }
+                    else
+                    {
+                        dialogText.text = dialogLines[currentLine];
+                    }
+                }
+                else
+                {
+                    dialogJustStarted = false;
+                }
+            }
+        }
+        
+        
+    }
+
+    public void ActivateDialog(string[] newLines)
+    {
+        dialogLines = newLines;
+        currentLine = 0;
+        dialogText.text = dialogLines[currentLine];
+        dialogBox.SetActive(true);
+        dialogJustStarted = true;
+    }
+
+    public bool isDialogBoxActive()
+    {
+        return dialogBox.activeInHierarchy;
+    }
+}
---
+++ Assets/Scripts/Dialog/DialogHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class DialogHandler : MonoBehaviour
+{
+    public string[] lines;
+    private bool canActivateDialog;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canActivateDialog && Input.GetButtonDown("Fire1") && !DialogController.instance.isDialogBoxActive())
+        {            
+            DialogController.instance.ActivateDialog(lines);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canActivateDialog = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            canActivateDialog = false;
+    }
+}
