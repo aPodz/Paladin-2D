a40d579763d35bdd56d90957a5d41032707b3b6a
aPodz <adam.podzim@gmail.com>
2024-04-07 11:34:45 +0200

    Created an ItemManager script

---
+++ Assets/Scripts/Items Management/ItemsManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsManager : MonoBehaviour
+{
+    public enum ItemType { Item, Weapon, Armor}
+    public ItemType itemType;
+    public enum EffectType { HP, Energy, Strength }
+    public EffectType effectType;
+
+    public string itemName, itemDesc;
+    public int itemValue;
+    public Sprite itemImage;
+    public int itemEffect;
+    
+    public int weaponAP, weaponStr, armorDef;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
