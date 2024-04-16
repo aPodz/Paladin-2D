a531032ed17e30b4459be896895a2faba205b346
aPodz <adam.podzim@gmail.com>
2024-04-16 15:54:15 +0200

    Removed some extra item sprites that proabably won't be used

---
+++ Assets/Scripts/Items Management/ItemAssets.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAssets : MonoBehaviour
+{
+    public static ItemAssets instance;
+    [SerializeField] ItemsManager[] itemsAvailable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+
+    public ItemsManager GetItemAsset(string itemToGet)
+    {
+        foreach (ItemsManager item in itemsAvailable)
+        {
+            if (item.itemName == itemToGet)
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+
+}
