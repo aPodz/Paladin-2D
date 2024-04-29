c6dd76f7ea376aafc2af4e469e5f60cd3e1256f0
aPodz <adam.podzim@gmail.com>
2024-04-29 18:18:05 +0200

    Created a battleType script to prepare battles easier and a battle initator that will be set to a zone trigger to start a battle

---
+++ Assets/Scripts/Battle System/BattleType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BattleType
+{
+    public string[] enemies;
+    public int XPReward;
+    public ItemsManager[] rewardedItems;
+    public AudioManager battleMusic;
+}
