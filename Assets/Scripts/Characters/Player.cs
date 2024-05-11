c908342dc90e77b2405c85325f93414ee720538e
aPodz <adam.podzim@gmail.com>
2024-05-11 20:19:06 +0200

    Made some final adjustments and fixes

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
